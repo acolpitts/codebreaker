37cda2d546a1aa4f8da68cfa1e1f3117ea025c79
Adam Colpitts <adam@completeperspective.com>
2012-03-07 15:25:24 -0800

    code-breaker submits guess feature red

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -4,17 +4,17 @@ module Codebreaker
   describe Game do
     describe "#start" do
       let(:output) { double('output').as_null_object }
-      let(:game) = { Game.new(@output) }
+      let(:game) { Game.new(output) }
       
       
       it "sends a welcome message" do
         output.should_receive(:puts).with('Welcome to Codebreaker!')
-        game.start
+        game.start('1234')
       end
       
       it "prompts for the first guess" do
         output.should_receive(:puts).with('Enter guess:')
-        game.start
+        game.start('1234')
       end
     end
   end
