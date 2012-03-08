873530881e22fbe930138688f38449b75c3dc490
Adam Colpitts <adam@completeperspective.com>
2012-03-07 17:50:39 -0800

    delivered code-breakers submits guess feature

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -3,16 +3,25 @@ class Marker
     @secret, @guess = secret, guess
   end
   
-  def exact_match_count()
+  def exact_match_count
     (0..3).inject(0) do |count, index|
       count + (exact_match?(index) ? 1 : 0)
     end
   end
   
-  def number_match_count()
-    (0..3).inject(0) do |count, index|
-      count + (number_match?(index) ? 1 : 0)
+  def number_match_count
+    total_match_count - exact_match_count
+  end
+  
+  def total_match_count
+    secret = @secret.split('')
+    @guess.split('').inject(0) do |count, n|
+      count + (delete_first(secret, n) ? 1 : 0)
     end
+  end
+  
+  def delete_first(code, n)
+    code.delete_at(code.index(n)) if code.index(n)
   end
   
   def exact_match?(index)
